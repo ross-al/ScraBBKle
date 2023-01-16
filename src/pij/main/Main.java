c657ce8066d741d3abae907b86cb24d80617c054
ross-al <115371010+ross-al@users.noreply.github.com>
2023-01-16 20:49:54 +0000

    testing code to read wordlist and verify if word

--- src/pij/main/Main.java
+++ src/pij/main/Main.java
@@ -1,8 +1,15 @@
 package pij.main;
 
 public class Main {
+    public static void main(String[] args) {
+        Main launcher = new Main();
+        launcher.launch();
+    }
 
-    //create launcher to separate main
-    //run programme in launcher
-
+    public void launch() {
+        System.out.println("hello world");
+        Word word = new Word();
+        System.out.println(word.isWord()); //need to create instance of word first
+        //gameplay code here
+    }
 }
