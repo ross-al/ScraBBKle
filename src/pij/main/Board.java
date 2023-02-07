b221d03774213616ea209c03ece8ac2c878288cb
ross-al <115371010+ross-al@users.noreply.github.com>
2023-02-07 17:29:16 +0000

    changed Board class to interface and created GameBoard class for scrabbkle board

--- src/pij/main/Board.java
+++ src/pij/main/Board.java
@@ -1,155 +1,18 @@
 package pij.main;
 
-import java.io.*;
-import java.util.Scanner;
+import java.io.File;
+
+public interface Board {
+
+    String[][] convertToBoard(String[][] board);
+    //take type Square instead of String[][]
+    // should be able to take Tile
+    // should have premiumSquare or premiumWord member fields
+    File getInputFile();
+    int getBoardSize();
+    boolean isValidSize();
+    void printBoard(String[][] board);
+    String[][] getMyBoard();
 
 
-//get file input and put into array, must have row/col labels
-//check if file input is valid using method (i.e. size etc.)
-//create constructor for board using array
-//need a get file method for non-default boards
-
-//needs to either load the defaultBoard (see txt file already provided)
-//or needs to load a user-made board from a txt file
-
-//method to iterate through board and store letter squares and letter words in hash map?
-//would need to store index and bonus value
-
-//need to print board
-//need to update board with tiles (and print updated version)
-
-public class Board {
-    private File inputFile;
-    private int boardSize;
-    private int columns;
-    private int rows;
-    private String[][] myBoard;
-
-    public Board() {
-        this.inputFile = getInputFile();
-        this.boardSize = getBoardSize();
-        //prefer to handle if clause in method outside constructor...
-        if (!(isValidSize())) {
-            System.out.println();
-            System.out.println("Sorry, your board needs to be between 12-26 squares wide");
-            getInputFile();
-        }
-        rows = 1 + boardSize; //need to add +1 when I add a-z and 1-26 labels
-        columns = rows;
-        this.myBoard = new String[rows][columns];
-        convertToBoard(myBoard);
-    }
-
-    //change array type to generic or Tile?
-    //convert file into arraylist, and then array list into 2d array?
-    //ignore A[0][0] as this will have file size, so start from A[0][1]?
-    public String[][] convertToBoard(String[][] myBoard) {
-        myBoard[0][0] = "\t";
-        //this labelling doesn't work
-        char c = 'a';
-        for (int a = 1; a < myBoard.length; a++){
-            String d = String.valueOf(c);
-            myBoard[0][a] = "\t" + d;
-            c++;
-            }
-        for (int b = 1; b < myBoard.length; b++) {
-                String q = Integer.toString(b);
-                myBoard[b][0] = "\t" +q;
-            }
-        for (int i = 1; i < myBoard.length; i++) {
-            for (int j = 1; j < myBoard.length; j++) {
-                myBoard[i][j] = "\t" + "x";
-            }
-        }
-        System.out.println("File name: " + inputFile);
-        System.out.println("Board size: " + boardSize + "x" + boardSize);
-        System.out.println("Valid board size: " + isValidSize());
-        System.out.println("Your board is ready! GAME ON!");
-        System.out.println();
-        return myBoard;
-    }
-
-    //getinput file
-    //check size is ok
-    //read second line onwards
-    //iterate through to create array
-    //columns must be represented by letter a-z
-    //rows by numbers 1-26
-    //left most column is a
-    //top row is 1
-    //need to group strings e.g. {1} . (2)
-
-    public File getInputFile() {
-        String fileName = "./resources/defaultBoard.txt";
-        File inputFile = new File(fileName);
-        String defaultBoard = "d";
-        String loadBoard = "l";
-        System.out.println("Would you like to _l_oad a board or use the _d_efault board?");
-        System.out.println("Please enter your choice (l/d):");
-        System.out.println();
-        Scanner input = new Scanner(System.in);
-        String boardType = input.nextLine().toLowerCase();
-        while (!(boardType.equals(defaultBoard) || boardType.equals(loadBoard))) {
-            System.out.println();
-            System.out.println("Choice not recognised. Please enter your choice (l/d):");
-            boardType = input.nextLine().toLowerCase();
-        }
-        if (boardType.equals(loadBoard)) {
-            boolean legitFile = false;
-            while (!legitFile) {
-                System.out.println();
-                System.out.println("Please enter the file name of the board: ");
-                fileName = input.nextLine();
-                inputFile = new File(fileName);
-                if (inputFile.exists()) {
-                    legitFile = true;
-                    System.out.println();
-                    System.out.println("Thank you. Your file is loading...");
-                } else {
-                    System.out.println("The file you entered does not exist");
-                }
-            }
-        }
-        return inputFile;
-    }
-
-    //need to test this method with diff file size
-    public int getBoardSize() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
-            String text = reader.readLine();
-            try {
-                boardSize = Integer.parseInt(text);
-            } catch (NumberFormatException e) {
-                System.out.println("File not formatted correctly. First line must contain board size, e.g.'12'");
-                throw new RuntimeException(e);
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return boardSize;
-    }
-
-
-    public boolean isValidSize() {
-        return boardSize >= 12 && boardSize <= 26;
-    }
-
-
-    public void printBoard(String[][] board) {
-        System.out.println("Your current board: ");
-        System.out.println();
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                System.out.print(board[i][j]); //can we right-justify row nums?
-            }
-            System.out.println();
-        }
-    }
-
-
-    public String[][] getMyBoard() {
-        return myBoard;
-    }
 }
--- src/pij/main/Main.java
+++ src/pij/main/Main.java
@@ -1,5 +1,7 @@
 package pij.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Main launcher = new Main();
@@ -7,22 +9,19 @@ public class Main {
     }
 
     public void launch() {
-        System.out.println("hello world");
-        Tile myTile = new Tile('c', 3);
-        System.out.println(myTile.toString());
-        Tile myTile2 = new Tile('d', 2);
-        System.out.println(myTile2.toString());
-        //Word myWord = new Word("aa");
+        //System.out.println("hello world");
+        //Tile myTile = new Tile('c', 3);
+        //System.out.println(myTile.toString());
+        //Tile myTile2 = new Tile('d', 2);
+        //System.out.println(myTile2.toString());
+        //Word myWord = new Word("aa")
         //System.out.println(myWord.isWord());
-        WordList myWordList = new WordList();
-        System.out.println(myWordList.isWord("zzzzza"));
-        System.out.println(myWordList.isWord("zymurgies"));
-        System.out.println(myWordList.isWord("deflocculates"));
-        System.out.println(myWordList.isWord("fhwoifhvwvhwv"));
-        System.out.println(myWordList.isWord("qi"));
-        System.out.println(myWordList.isWord("QI"));
-        System.out.println(myWordList.isWord("defloration"));
+       // WordList myWordList = new WordList();
+        //System.out.println(myWordList.isWord("zzzzza"));
+        GameBoard board = new GameBoard();
+        board.printBoard(board.getMyBoard());
 
+        //myBoard.printBoard();
         //gameplay code here
 
         }
