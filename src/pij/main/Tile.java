bddb052c505cbcdcd13108c894e49402352a14aa
ross-al <115371010+ross-al@users.noreply.github.com>
2023-02-19 16:53:34 +0000

    added Letter and Value switch in Tile class to calculate tile value

--- src/pij/main/Tile.java
+++ src/pij/main/Tile.java
@@ -6,9 +6,9 @@ public class Tile {
     private char letter;
     private int value;
 
-    public Tile(char letter, int value){
+    public Tile(char letter){ //value should come from map
         this.letter = letter;
-        this.value = value;
+       this.value = calculateTileValue(letter); //need wildcard logic, needs small letter 'w' not 'W' when played
     }
 
     public char getLetter(){
@@ -20,17 +20,70 @@ public class Tile {
     }
 
     public String toString(){
-        char upperCaseLetter = Character.toUpperCase(letter); //we don't want this for WildCard tioles
+        char upperCaseLetter = Character.toUpperCase(letter); //we don't want this for WildCard tiles
         return "[" + upperCaseLetter + value + "]";
     }
 
+    public int calculateTileValue(char letter){ //need wildcard logic too
+        //need to cast to UpperCase?
+        int value;
+        switch (letter){
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'L':
+            case 'N':
+            case 'S':
+            case 'T':
+            case 'R':
+                value = 1;
+                break;
+            case 'D':
+            case 'G':
+                value = 2;
+                break;
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                value = 3;
+                break;
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                value = 4;
+                break;
+            case 'K':
+                value = 5;
+                break;
+            case 'J':
+            case 'X':
+                value = 8;
+                break;
+            case 'Q':
+            case 'Z':
+                value = 10;
+                break;
+            default:
+                value = 0;
+                break;
+        }
+        return value;
+    }
+
+
+
 }
 
-    //tile scores:
-    //(1 point)-A, E, I, O, U, L, N, S, T, R.
-    //(2 points)-D, G.
-    //(3 points)-B, C, M, P.
-    //(4 points)-F, H, V, W, Y.
-    //(5 points)-K.
-    //(8 points)- J, X.
-    //(10 points)-Q, Z.
+//tile values:
+//(1 point)-A, E, I, O, U, L, N, S, T, R.
+//(2 points)-D, G.
+//(3 points)-B, C, M, P.
+//(4 points)-F, H, V, W, Y.
+//(5 points)-K.
+//(8 points)- J, X.
+//(10 points)-Q, Z.
