d80a64b5de34938ee9962cccb822656d8462534f
ross-al <115371010+ross-al@users.noreply.github.com>
2023-02-19 15:16:00 +0000

    added printLabel member field to print row/colum labels or premiumWord/premiumLetter values

--- src/pij/main/Square.java
+++ src/pij/main/Square.java
@@ -3,26 +3,27 @@ package pij.main;
 public class Square {
 
     private Tile tile;
-    private String premiumLabel;
-    private int premiumLetter;
-    private int premiumWord;
+    protected String printLabel; //maybe just change to label so can have rows etc
+    private int premiumLetterValue;
+    private int premiumWordValue;
 
-    public Square(String premiumLabel){
+
+    public Square(String printLabel){
         tile = null;
-        this.premiumLabel = premiumLabel; //String from input file
-        premiumLetter = convertPremiumLetter(premiumLabel);
-        premiumWord = convertPremiumWord(premiumLabel);
+        this.printLabel = printLabel; //String from input file
+        premiumLetterValue = convertPremiumLetter(printLabel);
+        premiumWordValue = convertPremiumWord(printLabel);
     }
 
-    public int convertPremiumLetter(String premiumLabel){
+    public int convertPremiumLetter(String printLabel){
         String sub = String.valueOf(0);
         int value;
-        if (premiumLabel.contains("("){  //e.g. (3)
-            if (premiumLabel.charAt(2) == ')') {
-                sub = String.valueOf(premiumLabel.charAt(1)); //e.g. 3
+        if (printLabel.contains("(")){  //e.g. (3)
+            if (printLabel.charAt(2) == ')') {
+                sub = String.valueOf(printLabel.charAt(1)); //e.g. 3
             }
-            if (premiumLabel.charAt(2) != ')'){ //e.g. (99
-                sub = String.valueOf(premiumLabel.substring(1)); //e.g. 99
+            if (printLabel.charAt(2) != ')'){ //e.g. (99
+                sub = String.valueOf(printLabel.substring(1)); //e.g. 99
             }
 
          }
@@ -30,20 +31,28 @@ public class Square {
         return value;
     }
 
-    public int convertPremiumWord(String premiumWord){
+    public int convertPremiumWord(String printLabel){
         String sub = String.valueOf(0);
         int value;
-        if (premiumWord.contains("{"){  //e.g. {3}
-            if (premiumWord.charAt(2) == '}') {
-                sub = String.valueOf(premiumWord.charAt(1)); //e.g. 3
+        if (printLabel.contains("{")){  //e.g. {3}
+            if (printLabel.charAt(2) == '}') {
+                sub = String.valueOf(printLabel.charAt(1)); //e.g. 3
             }
-            if (premiumWord.charAt(2) != '}'){ //e.g. {99
-                sub = String.valueOf(premiumWord.substring(1)); //e.g. 99
+            if (printLabel.charAt(2) != '}'){ //e.g. {99
+                sub = String.valueOf(printLabel.substring(1)); //e.g. 99
             }
 
         }
         value = Integer.parseInt(sub);
         return value;
+    }
+
+    public int getPremiumLetterValue(){
+        return premiumLetterValue;
+    }
+
+    public int getPremiumWordValue(){
+        return premiumWordValue;
     }
 
 }
