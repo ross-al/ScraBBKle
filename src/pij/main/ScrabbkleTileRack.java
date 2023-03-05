eefeb470c3e2a1d21f4f7040fb41da79fe4b6399
ross-al <115371010+ross-al@users.noreply.github.com>
2023-03-05 18:18:43 +0000

    added printRank() method to TileRack

--- src/pij/main/ScrabbkleTileRack.java
+++ src/pij/main/ScrabbkleTileRack.java
@@ -1,6 +1,34 @@
 package pij.main;
 
+import java.util.ArrayList;
+
 public class ScrabbkleTileRack implements TileRack{
-    private ScrabbkleTile[][] myTileRack;
-    //code here
+    private ArrayList<ScrabbkleTile> myTileRack;
+
+    public ScrabbkleTileRack() {
+        myTileRack= new ArrayList<>();
+    }
+
+    @Override
+    public void addTile(ScrabbkleTile myTile) {
+        myTileRack.add(myTile);
+    }
+
+    @Override
+    public int getSize() {
+        return myTileRack.size();
+    }
+
+    public void printRank(){
+        for (int i =0; i<myTileRack.size(); i++ ){
+            System.out.print(myTileRack.get(i).getPrintInTileRackFormat());
+        }
+    }
+
+    // playTile(takes char as param)
+    //removes tile from list once played
+
+    //print arraylist tile.
+
+
 }
--- src/pij/main/TileRack.java
+++ src/pij/main/TileRack.java
@@ -1,6 +1,10 @@
 package pij.main;
 
 public interface TileRack {
+    void addTile(ScrabbkleTile myTile);
+    int getSize();
+    void printRank();
+
 
     //contains up to 7 tiles
     //players start by getting 7 random tiles from bag
