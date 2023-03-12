c1ff93cd98aa0553ef260858c5945b050f03fdcc
ross-al <115371010+ross-al@users.noreply.github.com>
2023-03-12 17:07:47 +0000

    passed board, wordList, tileBag as params in ScrabbklePlayer constructor

--- src/pij/main/ScrabbklePlayer.java
+++ src/pij/main/ScrabbklePlayer.java
@@ -2,28 +2,50 @@ package pij.main;
 
 import java.util.ArrayList;
 
-abstract class ScrabbklePlayer implements Player{
-    int score;
-    private ArrayList<ScrabbkleTile> myTileRack;//need to restrinct to 7 tiles
+public class ScrabbklePlayer implements Player{
+    private int score;
+    private ArrayList<ScrabbkleTile> myTileRack;//need to restrict to 7 tiles
+    private ScrabbkleBoard board;
+    private WordList wordList;
+    private ScrabbkleTileBag tileBag;
 
-    public ScrabbklePlayer(){
+    public ScrabbklePlayer(ScrabbkleBoard board, WordList wordList, ScrabbkleTileBag tileBag){
+        this.board = board;
+        this.wordList = wordList;
+        this.tileBag = tileBag;
         myTileRack = new ArrayList<>();
     }
 
+    public void fillRack(){
+        if (!tileBag.isEmpty()) {
+            while (myTileRack.size()<7){
+                addTileToRack();
+            }
+        }
+    }
+
     @Override
-    public void addTileToRack(ScrabbkleTile myTile) {
+    public void addTileToRack() {
+
+        ScrabbkleTile myTile = tileBag.getRandomTile();
         myTileRack.add(myTile);
     }
 
-    @Override
-    public int getRackSize() {
-        return myTileRack.size();
-    }
 
     public void printRack(){
-        for (int i =0; i < myTileRack.size(); i++ ){
-            System.out.print(myTileRack.get(i).getPrintInTileRackFormat());
+        System.out.println();
+        if (!(myTileRack.isEmpty())) {
+            System.out.println("It's your turn! Your tiles: ");
+            System.out.print(myTileRack.get(0).getPrintInTileRackFormat());
+            for (int i = 1; i < myTileRack.size(); i++) {
+                System.out.print(", " + myTileRack.get(i).getPrintInTileRackFormat());
+            }
         }
+        else {
+            System.out.println("Your rack is empty");
+        }
+        System.out.println();
+        System.out.println();
     }
 
     @Override
@@ -47,5 +69,9 @@ abstract class ScrabbklePlayer implements Player{
         //HumanPlayer first move MUST be in centre square
         //if board is even, then top left square in centre is centre square
         return null;
+    }
+
+    public ScrabbkleBoard getBoard(){
+        return board;
     }
 }
