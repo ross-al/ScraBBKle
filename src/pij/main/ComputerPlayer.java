489624c5e5d4326963b73f8a63da9caac2368ae2
ross-al <115371010+ross-al@users.noreply.github.com>
2023-03-12 17:09:48 +0000

    added first attempt at managing game play within player subclasses

--- src/pij/main/ComputerPlayer.java
+++ src/pij/main/ComputerPlayer.java
@@ -1,6 +1,15 @@
 package pij.main;
 
+
+//REDUNDANT CLASS???
+
 public class ComputerPlayer extends ScrabbklePlayer {
 
+    public ComputerPlayer(ScrabbkleBoard board, WordList wordList, ScrabbkleTileBag tileBag) {
+        super(board, wordList, tileBag);
+    }
 
+    public void playMove() {
+        fillRack();
+    }
 }
--- src/pij/main/HumanPlayer.java
+++ src/pij/main/HumanPlayer.java
@@ -1,17 +1,33 @@
 package pij.main;
 
-
+import java.util.Scanner;
 
 public class HumanPlayer extends ScrabbklePlayer {
-    String playerName;
 
-    public HumanPlayer(String name){
-        this.playerName = name;
+    public HumanPlayer(ScrabbkleBoard board, WordList wordList, ScrabbkleTileBag tileBag) {
+        super(board, wordList, tileBag);
     }
 
-    public String getName(){
-        return playerName;
+    public void playMove() {
+        getBoard().printBoard();
+        fillRack();
+        printRack();
+        inputPlayerWord();
+        //check if word is possible (should be in super)
+        //check if had wildcard (should be in super?, or limit wildcard to human)
+        //play word (should be in super)
+        //getwordscore (should be in super)
     }
+
+    public String inputPlayerWord(){
+        System.out.println("Please enter your move with letter sequence, position, and");
+        System.out.println("direction (d for down, r for right) separated by commas.");
+        System.out.println("Entering just two commas passes.");
+        System.out.println();
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
+    }
+
 
     //use wildCard()
     //get string input
