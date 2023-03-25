279dc51bb6962d7f44b34b80aea94226c14df977
ross-al <115371010+ross-al@users.noreply.github.com>
2023-03-25 13:34:46 +0000

    added 2D array moveSquares to store indexes for squares for each move

--- src/pij/main/HumanPlayer.java
+++ src/pij/main/HumanPlayer.java
@@ -1,15 +1,19 @@
 package pij.main;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HumanPlayer extends ScrabbklePlayer {
     private String moveWord;
     private String movePosition;
     private String moveDirection;
+    private ArrayList<Character> charsInWord;
+    private int[][] moveSquares;
     private boolean firstMove = true;
 
     public HumanPlayer(ScrabbkleBoard board, ScrabbkleWordList wordList, ScrabbkleTileBag tileBag) {
         super(board, wordList, tileBag);
+        charsInWord = new ArrayList<>();
     }
 
     public void playMove() {
@@ -22,30 +26,33 @@ public class HumanPlayer extends ScrabbklePlayer {
                 moveWord = instructions[0];
                 movePosition = instructions[1];
                 moveDirection = instructions[2];
+                //split each letter in word into character
+                splitWordToChar(moveWord);
+                calculateMoveSquares(moveWord, movePosition, moveDirection);
                 //check if valid word, position and direction
-                //if (isValidMove()) {
+                if (isValidMove()) {
                    validMove = true;
                     //rest of code here?????
-
 
                     //print move summary, scores and updated board
                    System.out.print("The move is: " + moveWord);
                    System.out.print(" at position " + movePosition + ",");
                    System.out.print(" direction: "+ getMoveDirection());
-                //} else {
-                    //System.out.println("This is not a valid move");
-               // }
+                   //remove all chars in list after each move
+                   charsInWord.clear();
+
+                   //NEED TO CLEAR MOVESQUARES 2D ARRAY TOO???
+                    //OR GETS OVERRIDDEN EACH TIME METHOD IS CALLED?
+
+                } else {
+                    System.out.println("This is not a valid move");
+                }
             } else {
                 //skip move
-               // validMove = true;
                 return;
             }
         }
    }
-
-
-
-
 
             //check if correct tiles in rack
             //check if correct tiles on board already
@@ -87,7 +94,10 @@ public class HumanPlayer extends ScrabbklePlayer {
 
 
     public boolean isValidMove(){
-        return (isValidWord(moveWord) && isValidPosition(movePosition) && isValidDirection(moveDirection));
+        return (isValidWord(moveWord)
+                && isValidPosition(movePosition)
+                && isValidDirection(moveDirection));
+                //&& hasAllTilesAvailable(charsInWord);
     }
 
     public boolean isValidWord(String str){
@@ -126,9 +136,67 @@ public class HumanPlayer extends ScrabbklePlayer {
         return (firstMove);
     }
 
+    //split given word into character arrayList
+    public void splitWordToChar(String moveWord){
+        for (int i = 0; i < moveWord.length(); i++){
+            char c = moveWord.charAt(i);
+            charsInWord.add(c);
+        }
+    }
     //check if tiles are available in rack to make desired word
-    public boolean hasTilesAvailable(){
-        return true;
+
+    public boolean hasAllTilesAvailable(ArrayList<Character> charsInWord){
+        //boolean allTilesAvailable= false; //MIGHT NOT NEED
+        ArrayList<ScrabbkleTile> tileRack = getTileRack(); //gets tileRack from super
+        //first check if characters exist in tile rack
+        for (Character character : charsInWord) {
+            for (ScrabbkleTile scrabbkleTile : tileRack) {
+                if (character == scrabbkleTile.getLetter()) {
+                    return true;
+                    //if not, check if characters already exist on board
+                } else {
+                    for(int i = 0; i < moveSquares.length; i++){
+                        for(int j = 0; j < moveSquares.length; j++){
+                            int col = moveSquares[i][j];
+
+                            //will give error if null???? try catch?
+                            //getTile, if no tile, then false
+                            //if tile present, get letter
+                            char letter = super.getBoard().getBoard()[col][row].getTile().getLetter();
+                            int row =
+                            if(character == letter)
+                                    //do stuff
+                                return true;
+                            }
+                        }
+
+                    for (int i = 1; i < wordLength; i++) {
+                        moveSquares[i][0] = startCol + i;
+                    }
+                    for (int j = 0; j < wordLength; j++){
+                        moveSquares[j][1] = startCol;
+                    }
+                    //check if on board
+
+                       //check if squares in moveSquares have tiles
+                    }
+                    //need to get word start position
+                    //need word length
+                    //need word direction
+                    //then calculate index
+
+                }
+            }
+            //check if char in charsInWord list matches tile letter in player tileRack
+
+            //check if char in tile in rack
+            //else check if on board, in direction of move (need indexes)
+            //return true
+        }
+        //wildcard, if char lowercase, then if char is empty in rack, or lowercase on board
+        //check if tiles with char are on board already
+        //if not check if tiles are in rack
+        return allTilesAvailable;
     }
 
     //methods used to print move summary in console
@@ -140,15 +208,15 @@ public class HumanPlayer extends ScrabbklePlayer {
         }
     }
 
-   //REMOVE IF REDUNDANT ??????????
+/*   //REMOVE IF REDUNDANT ??????????
     public String getMoveWord(){
         return moveWord;
-    }
+    }*/
 
-    //used for testing
+/*    //used for testing
     public String getMovePosition(){
         return movePosition;
-    }
+    }*/
 
     public int getPositionColumn(String movePosition){
         char position = movePosition.charAt(0);
@@ -167,12 +235,49 @@ public class HumanPlayer extends ScrabbklePlayer {
         return Integer.parseInt(str);
     }
 
+    public void calculateMoveSquares(String moveWord, String movePosition, String moveDirection){
+        //create int[][] to store row and column positions for each tile on board
+        int wordLength = moveWord.length();
+        moveSquares = new int[wordLength][2];
+        //add movePosition in first 2 elements
+        int startCol = getPositionColumn(movePosition);
+        int startRow = getPositionRow(movePosition);
+        moveSquares[0][0] = startCol;
+        moveSquares[0][1] = startRow;
+        //if word direction is down then column stays the same and row increments
+        if(moveDirection.equals("d")) {
+            for (int i = 1; i < wordLength; i++) {
+                moveSquares[i][0] = startCol;
+            }
+            for (int j = 0; j < wordLength; j++){
+                moveSquares[j][1] = startRow + j;
+            }
+            //if word direction is right then row stays the same and col increments
+        } else {
+            for (int i = 1; i < wordLength; i++) {
+                moveSquares[i][0] = startCol + i;
+            }
+            for (int j = 0; j < wordLength; j++){
+                moveSquares[j][1] = startCol;
+            }
+
+            //reminder of 2d array index
+              /*  a[0][0] a[0][1]
+                  a[1][0] a[1][1]
+                  a[2][0] a[2][1]
+                  a[3][0] a[3][1] */
+        }
+    }
 
 
     public boolean isValidDirection(String moveDirection){
         //PLACEHOLDER !!!!!!!!!!!!!!
-        //need to create logic here to get direciton is valid!!!!
+        //need to create logic here to get direction is valid!!!!
         return true;
+    }
+
+    public int[][] getMoveSquares(){
+        return moveSquares;
     }
 }
 
