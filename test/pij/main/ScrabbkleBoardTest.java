30748d13dcfc27bcfb4077a7db761553be53d748
ross-al <115371010+ross-al@users.noreply.github.com>
2023-03-27 19:57:26 +0100

    added jUnit tests to verify print format for default board

---
+++ test/pij/main/ScrabbkleBoardTest.java
@@ -0,0 +1,65 @@
+package pij.main;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScrabbkleBoardTest {
+    ScrabbkleBoard board;
+
+    @BeforeEach
+    void buildUp(){
+        board = new ScrabbkleBoard();
+        board.setBoardSize(15);
+        String fileName = "./resources/defaultBoard.txt";
+        File inputFile = new File(fileName);
+        board.convertToBoard(inputFile);
+    }
+
+    @Test
+    void shouldReturnLetterAFromColumnLabels() {
+        String expected = String.valueOf('a');
+        String actual = board.getBoard()[0][1].getPrintLabel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnLetterOFromColumnLabels(){
+        String expected = String.valueOf('o');
+        String actual = board.getBoard()[0][15].getPrintLabel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnInt1(){
+        int expected = 1;
+        int actual = Integer.parseInt(board.getBoard()[1][0].getPrintLabel());
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void shouldReturnInt15(){
+        int expected = 15;
+        int actual = Integer.parseInt(board.getBoard()[15][0].getPrintLabel());
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldReturnPremiumLetterPrintLabel(){
+        String expected = "{3}";
+        String actual = board.getBoard()[1][1].getPrintLabel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnPremiumWordPrintLabel(){
+        String expected = "(2)";
+        String actual = board.getBoard()[1][4].getPrintLabel();
+        assertEquals(expected, actual);
+    }
+
+}
